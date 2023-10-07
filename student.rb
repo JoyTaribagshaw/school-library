fc0dc24c0ff064dbc68e92767c8169d37e874d18
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-07 17:34:31 +0100

    fix linter errors

--- student.rb
+++ student.rb
@@ -1,5 +1,3 @@
-require_relative 'person'
-
 # class for student
 class Student < Person
   attr_accessor :classroom
@@ -9,16 +7,7 @@ class Student < Person
     @classroom = classroom
   end
 
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
-  end
-
   def play_hooky?
     '¯\(ツ)/¯'
   end
 end
-
-student1 = Student.new(10, 'class', 'mag')
-puts student1.of_age?
-puts student1.can_use_services?
